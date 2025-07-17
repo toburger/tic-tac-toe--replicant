712c0de50f2fa6624d92c09aae746d74d9cf8f13
Tobias Burger <tobias_burger@hotmail.com>
2025-07-17 14:41:06 +0200

    Initial commit

---
+++ src/tic_tac_toe/app.cljs
@@ -0,0 +1,34 @@
+(ns tic-tac-toe.app
+  (:require [gadget.inspector :as inspector]
+            [replicant.dom :as r]))
+
+(defonce ^:private !state (atom 0))
+
+(defn- main-view [state]
+  [:div
+   [:button {:type "text" :on {:click [[:decrement 2]]}} "--"]
+   [:button {:type "text" :on {:click [[:decrement 1]]}} "-"]
+   [:span (str state)]
+   [:button {:type "text" :on {:click [[:increment 1]]}} "+"]
+   [:button {:type "text" :on {:click [[:increment 2]]}} "++"]])
+
+(defn- render! [state]
+  (r/render
+   (js/document.getElementById "app")
+   (main-view state)))
+
+(defn- event-handler [{:replicant/keys [^js js-event] :as replicant-data} actions]
+  (doseq [action actions]
+    (let [[action-name & args] action]
+      (case action-name
+        :increment (swap! !state #(+ % (first args)))
+        :decrement (swap! !state #(- % (first args))))))
+  (render! @!state))
+
+(defn ^{:dev/after-reload true :export true} start! []
+  (render! @!state))
+
+(defn ^:export init! []
+  #_(inspector/inspect "App state" !state)
+  (r/set-dispatch! event-handler)
+  (start!))
